2a6448271b4398b722bc67cd811536c79086b501
matiasmartin <matiass.lean@gmail.com>
2024-08-04 14:44:17 +0200

    feat: first version

---
+++ src/main/java/com/proyectum/ddd/aggregate/Aggregate.java
@@ -0,0 +1,34 @@
+package com.proyectum.ddd.aggregate;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
+
+public abstract class Aggregate<ID extends ValueObject<UUID>> {
+
+    private ID id;
+    private final List<DomainEvent> events;
+
+    private Aggregate() {
+        this.events = new ArrayList<>();
+    }
+
+    protected Aggregate(final ID id) {
+        this();
+        this.id = id;
+    }
+
+    public ID getId() {
+        return this.id;
+    }
+
+    public List<DomainEvent> getDomainEvents() {
+        return Collections.unmodifiableList(this.events);
+    }
+
+    protected void registerEvent(DomainEvent event) {
+        assert event != null;
+        this.events.add(event);
+    }
+}
---
+++ src/main/java/com/proyectum/ddd/aggregate/ValueObject.java
@@ -0,0 +1,6 @@
+package com.proyectum.ddd.aggregate;
+
+public interface ValueObject<T> {
+
+    T value();
+}
